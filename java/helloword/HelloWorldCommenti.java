f9d1ad3ccb84972a93f28cd3b0e923e495f4ffab
Sergio Arellanom <sergioarellanodiaz@gmail.com>
2023-05-02 13:45:36 +0200

    cambio nome file

---
+++ java/helloword/HelloWorldCommenti.java
@@ -0,0 +1,14 @@
+/*
+ * ¡Hola! El código que me compartiste es un ejemplo de un programa Java que imprime “Hello, World!” 
+ * en la consola. Este programa es muy común y se utiliza para introducir a los principiantes al lenguaje 
+ * de programación Java.
+ * El programa consta de una clase llamada “HelloWorld” que tiene un método llamado “main”. 
+ * El método “main” es el punto de entrada del programa y se ejecuta cuando se inicia el programa. 
+ * El método “main” utiliza el objeto “System.out” para imprimir el mensaje “Hello, World!” en la consola.
+ */
+public class HelloWorldCommenti {
+    public static void main(String[] args) {
+        // imprimir el mensaje “Hello, World!” en la consola
+        System.out.println("Hello, World!");
+    }
+}
